b578008e385566da14ec91f936eba3481c4072cc
Thierry Thevenet <thierry.thevenet@talao.io>
2024-06-09 16:52:22 +0200

    update

--- routes/oidc4vci_api.py
+++ routes/oidc4vci_api.py
@@ -186,7 +186,7 @@ def credential_issuer_openid_configuration(issuer_id, mode):
             'authorization_endpoint': mode.server + 'issuer/' + issuer_id + '/authorize',
             'token_endpoint': mode.server + 'issuer/' + issuer_id + '/token',
             'jwks_uri':  mode.server + 'issuer/' + issuer_id + '/jwks',
-            #'pushed_authorization_request_endpoint' : mode.server +'issuer/' + issuer_id + '/authorize/par' 
+            'pushed_authorization_request_endpoint' : mode.server +'issuer/' + issuer_id + '/authorize/par' 
         })
         #if issuer_data['profile'] in ["HAIP", "POTENTIAL"]:
         #    as_config["require_pushed_authorization_requests"] = True
@@ -233,7 +233,7 @@ def as_openid_configuration(issuer_id, mode):
         'authorization_endpoint': mode.server + 'issuer/' + issuer_id + '/authorize',
         'token_endpoint': mode.server + 'issuer/' + issuer_id + '/token',
         'jwks_uri':  mode.server + 'issuer/' + issuer_id + '/jwks',
-        #'pushed_authorization_request_endpoint': mode.server +'issuer/' + issuer_id + '/authorize/par' 
+        'pushed_authorization_request_endpoint': mode.server +'issuer/' + issuer_id + '/authorize/par' 
     }
     #if issuer_data['profile'] in ['HAIP', 'POTENTIAL']:
     #    config['require_pushed_authorization_requests'] = True
