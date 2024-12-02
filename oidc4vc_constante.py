d1845b1cd83d373b163b6a0eafc38cecc45b29d0
Thierry Thevenet <thierry.thevenet@talao.io>
2024-12-02 08:58:09 +0100

    update

--- oidc4vc_constante.py
+++ oidc4vc_constante.py
@@ -50,7 +50,9 @@ predefined_presentation_uri_list = {
     'presentation_definition/insurancenaturalperson': 'Insurance Natural Person',
     'presentation_definition/sicpa': 'Sicpa',
     'presentation_definition/two_sd_jwt': 'Present 2 sd_jwt',
-    'presentation_definition/employee_badge': 'Employee Badge'
+    'presentation_definition/employee_badge': 'Employee Badge',
+    'presentation_definition/custom_employee': 'Employee Dentsu'
+
 
 
 }
--- routes/waltid_server.py
+++ routes/waltid_server.py
@@ -23,44 +23,11 @@ sess.init_app(app)
 qrcode = QRcode(app)
 
 
-
 def init_app(app, red, mode):
     app.add_url_rule('/sandbox/issuer/waltid/test',  view_func=waltid, methods=['GET', 'POST'])
     return
 
-
-
-payload1 = {
-    "issuerKey": {
-        "type": "jwk",
-        "jwk": {
-            "kty": "EC",
-            "d": "uTIT47GfSlRa0Da4CsyoIZpjjwQLFxmL2qmBuzZpEy0",
-            "crv": "P-256",
-            "kid": "FsHUZY4_tDJDvxdp5B6moS1kwpP7PBekw4KfK7m0LCU",
-            "x": "keR9l4u1SaZKMZ7wHvj_3z44vP0sa3nlzrnc8UjpQV0",
-            "y": "pmcaedg5dtc2R6ZPZfWCBY56_M_5fUZgsz4LWD0mG8U"
-        } 
-    },
-    "credentialConfigurationId": "UniversityDegree_jwt_vc_json",
-    "credentialData":{
-            "@context": [
-                "https://www.w3.org/2018/credentials/v1", "https://www.w3.org/2018/credentials/examples/v1"
-            ],
-            "type": ["VerifiableCredential", "UniversityDegreeCredential"],
-            "credentialSubject": {
-                "degree":{
-                    "type": "BachelorDegree",
-                    "name": "Bachelor of Science and Arts"
-                }
-            }
-        },
-    "authenticationMethod": "PRE_AUTHORIZED",
-    "issuerDid": "did:jwk:eyJrdHkiOiJFQyIsImNydiI6IlAtMjU2Iiwia2lkIjoiRnNIVVpZNF90REpEdnhkcDVCNm1vUzFrd3BQN1BCZWt3NEtmSzdtMExDVSIsIngiOiJrZVI5bDR1MVNhWktNWjd3SHZqXzN6NDR2UDBzYTNubHpybmM4VWpwUVYwIiwieSI6InBtY2FlZGc1ZHRjMlI2WlBaZldDQlk1Nl9NXzVmVVpnc3o0TFdEMG1HOFUifQ"
-}  
-
-
-payload2 = {
+payload = {
     "issuerKey": {
         "type": "jwk",
         "jwk": {
