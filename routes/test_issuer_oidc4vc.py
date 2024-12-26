a58e1da14d96eecdf59bd460476706c142601246
Thierry Thevenet <thierry.thevenet@talao.io>
2024-12-26 13:28:49 +0100

    UPdate

--- routes/test_issuer_oidc4vc.py
+++ routes/test_issuer_oidc4vc.py
@@ -221,7 +221,7 @@ def test_4(mode):
     issuer_id = issuer_test(4, mode)
     client_secret = issuer_test(4, mode, secret = True)
 
-    offer = ["OpenBadgeCredential", "TestCredential", "TestCredential2"]
+    offer = ["OpenBadgeCredential", "TestCredential", "TestCredential2",  "DBCGuest", "EmailPass"]
     headers = {
         'Content-Type': 'application/json',
         'X-API-KEY': client_secret
