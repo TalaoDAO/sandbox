aa32515c302ecf050d05afe45449e3f3948fa1e8
Thierry Thevenet <thierry.thevenet@talao.io>
2025-04-29 09:32:35 +0200

    update

--- routes/oidc4vci_api.py
+++ routes/oidc4vci_api.py
@@ -519,25 +519,19 @@ def oidc_issuer_landing_page(issuer_id, stream_id, red, mode):
     
     # https://app.talao.co/app/download/offer?key1=val1
 
-    if issuer_id in [
-        "raamxepqex", # test local
-        "tdiwmpyhzc" # test aws
-    ]:
-        deeplink_talao = 'talao-openid-credential-offer://?' + urlencode({'credential_offer': json.dumps(offer)})
-        deeplink_altme = 'altme-openid-credential-offer://?' + urlencode({'credential_offer': json.dumps(offer)})
+    #if issuer_id in [
+    #    "raamxepqex", # test local
+    #    "tdiwmpyhzc" # test aws
+    #]:
+    #    deeplink_talao = 'talao-openid-credential-offer://?' + urlencode({'credential_offer': json.dumps(offer)})
+    #    deeplink_altme = 'altme-openid-credential-offer://?' + urlencode({'credential_offer': json.dumps(offer)})
+    #else:
+    #    deeplink_altme = mode.deeplink_altme + 'app/download/oidc4vc?' + urlencode({'uri': url_to_display})
+    #    deeplink_talao = mode.deeplink_talao + 'app/download/oidc4vc?' + urlencode({'uri': url_to_display})
     
-    #elif issuer_id in [
-    #    "mslmgnysdh", # emailpass and phone pass
-    #    "acnliwayop",
-    #    "lpiqylqrrs",
-    #"]:
-    #    deeplink_talao = 'openid-credential-offer://?' + urlencode({'credential_offer': json.dumps(offer)})
-    #    deeplink_altme = 'openid-credential-offer://?' + urlencode({'credential_offer': json.dumps(offer)})
-    #
-    else:
-        deeplink_altme = mode.deeplink_altme + 'app/download/oidc4vc?' + urlencode({'uri': url_to_display})
-        deeplink_talao = mode.deeplink_talao + 'app/download/oidc4vc?' + urlencode({'uri': url_to_display})
-        
+    deeplink_talao = 'openid-credential-offer://?' + urlencode({'credential_offer': json.dumps(offer)})
+    deeplink_altme = 'openid-credential-offer://?' + urlencode({'credential_offer': json.dumps(offer)})
+     
     qrcode_page = issuer_data.get('issuer_landing_page')
     logging.info('QR code page file = %s', qrcode_page)
     return render_template(
