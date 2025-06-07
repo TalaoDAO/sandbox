e57186734bd8c1e2b2faa0d8b3ce87a21be35899
Thierry Thevenet <thierry.thevenet@talao.io>
2025-06-07 18:21:45 +0200

    update

--- profiles/digital_credential_marketplace.py
+++ profiles/digital_credential_marketplace.py
@@ -25,52 +25,51 @@ TALAO_ISSUER = {
                     "resident_country",
                 ],
                 "claims": {
-                        "resident_address": {
-                            "value_type": "string",
-                            "display": [{"name": "Address", "locale": "en-US"},
-                                        {"name": "Adresse", "locale": "fr-FR"}],
-                        },
-                        "resident_state": {
-                            "value_type": "string",
-                            "display": [{"name": "State", "locale": "en-US"},
-                                        {"name": "Région", "locale": "fr-FR"}],
-                        },
-                        "resident_city": {
-                            "value_type": "string",
-                            "display": [{"name": "City", "locale": "en-US"},
-                                        {"name": "Ville", "locale": "fr-FR"}],
-                        },
-                        "resident_street": {
-                            "value_type": "string",
-                            "display": [{"name": "Street", "locale": "en-US"},
-                                        {"name": "Rue", "locale": "fr-FR"}],
-                        },
-                        "resident_postal_code": {
-                            "value_type": "string",
-                            "display": [{"name": "Postal code", "locale": "en-US"},
-                                        {"name": "code postal", "locale": "fr-FR"}],
-                        },
-                        "resident_country": {
-                                "value_type": "bool",
-                                "display": [
-                                    {"name": "Country", "locale": "en-US"},
-                                    {"name": "Pays", "locale": "fr-FR"}],
-                        },
-                        "issuance_date": {
-                            "value_type": "string",
-                            "display": [{"name": "Issuance date", "locale": "en-US"},
-                                        {"name": "Délivré le", "locale": "fr-FR"}],
-                        },
-                        "issuing_country": {
-                            "value_type": "string",
-                            "display": [{"name": "Issuing country", "locale": "en-US"},
-                                        {"name": "Pays d'emission", "locale": "fr-FR"}],
-                        },
-                        "issuing_authority": {
-                            "value_type": "string",
-                            "display": [{"name": "Issuing authority", "locale": "en-US"},
-                                        {"name": "Authorité", "locale": "fr-FR"}],
-                        }
+                    "resident_address": {
+                        "value_type": "string",
+                        "display": [{"name": "Address", "locale": "en-US"},
+                                    {"name": "Adresse", "locale": "fr-FR"}],
+                    },
+                    "resident_state": {
+                        "value_type": "string",
+                        "display": [{"name": "State", "locale": "en-US"},
+                                    {"name": "Région", "locale": "fr-FR"}],
+                    },
+                    "resident_city": {
+                        "value_type": "string",
+                        "display": [{"name": "City", "locale": "en-US"},
+                                    {"name": "Ville", "locale": "fr-FR"}],
+                    },
+                    "resident_street": {
+                        "value_type": "string",
+                        "display": [{"name": "Street", "locale": "en-US"},
+                                    {"name": "Rue", "locale": "fr-FR"}],
+                    },
+                    "resident_postal_code": {
+                        "value_type": "string",
+                        "display": [{"name": "Postal code", "locale": "en-US"},
+                                    {"name": "code postal", "locale": "fr-FR"}],
+                    },
+                    "resident_country": {
+                            "value_type": "bool",
+                            "display": [
+                                {"name": "Country", "locale": "en-US"},
+                                {"name": "Pays", "locale": "fr-FR"}],
+                    },
+                    "issuance_date": {
+                        "value_type": "string",
+                        "display": [{"name": "Issuance date", "locale": "en-US"},
+                                    {"name": "Délivré le", "locale": "fr-FR"}],
+                    },
+                    "issuing_country": {
+                        "value_type": "string",
+                        "display": [{"name": "Issuing country", "locale": "en-US"},
+                                    {"name": "Pays d'emission", "locale": "fr-FR"}],
+                    },
+                    "issuing_authority": {
+                        "value_type": "string",
+                        "display": [{"name": "Issuing authority", "locale": "en-US"},
+                                    {"name": "Authorité", "locale": "fr-FR"}],
                     }
                 },
                 "cryptographic_binding_methods_supported": ["did", "jwk"],
@@ -94,9 +93,9 @@ TALAO_ISSUER = {
                         "background_color": "#14107c",
                         "text_color": "#FFFFFF",
                     }
-                ],
-            },
-          
+                ]
+            }
+        },
         "grant_types_supported": [
             "authorization_code",
             "urn:ietf:params:oauth:grant-type:pre-authorized_code",
