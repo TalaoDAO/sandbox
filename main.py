05b903abaf96f31ccb25978a37bc329d1417aab9
Thierry Thevenet <thierry.thevenet@talao.io>
2025-08-20 18:41:53 +0200

    updat

--- main.py
+++ main.py
@@ -619,6 +619,7 @@ def qrcode_wallet():
     qrcode_base64 = request.form.get("qrcode")
     if not qrcode_base64:
         return jsonify({"error": "missing qrcode"}), 400
+    model = request.form.get('oidc4vciDraft', "escalation")
     oidc4vciDraft = request.form.get('oidc4vciDraft')
     oidc4vpDraft = request.form.get('oidc4vpDraft')
     profil = request.form.get('profil', 'custom')
@@ -627,7 +628,7 @@ def qrcode_wallet():
     except Exception:
         return jsonify({"error": "invalid base64 format"}), 400
     try:
-        report = AI_Agent.analyze_qrcode(qrcode_str, oidc4vciDraft, oidc4vpDraft, profil, 'wallet QR code', "flash")
+        report = AI_Agent.analyze_qrcode(qrcode_str, oidc4vciDraft, oidc4vpDraft, profil, 'wallet QR code', model)
     except Exception as e:
         logging.error("Error in analyze_qrcode: %s", e)
         return jsonify({"error": "internal processing error"}), 500
@@ -643,6 +644,7 @@ def vc_wallet():
     if api_key not in ai_api_keys:
         return jsonify({"error": "access denied"}), 403
     vc_base64 = request.form.get("vc")
+    model = request.form.get('oidc4vciDraft', "escalation")
     if not vc_base64:
         return jsonify({"error": "missing qrcode"}), 400
     try:
@@ -650,7 +652,7 @@ def vc_wallet():
     except Exception:
         return jsonify({"error": "invalid base64 format"}), 400
     try:
-        report = AI_Agent.process_vc_format(vc_str, "8", "1.1", "wallet VC", "flash")
+        report = AI_Agent.process_vc_format(vc_str, "8", "1.1", "wallet VC", model)
     except Exception as e:
         logging.error("Error in analyze_qrcode: %s", e)
         return jsonify({"error": "internal processing error"}), 500
